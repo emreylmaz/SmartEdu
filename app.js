7f8243afd04e4c60af9c36994e4ec3b17bbb68f9
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-03-24 00:43:56 +0300

    24.03.2022T00:43am update

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ const express = require('express');
 
 const app = express();
 
+//Tamplate engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+
 const port = process.env.PORT || 3000;
 
-
+//Routes
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
+    res.status(200).render('index', {
+      page_name: 'index',
+    });
 });
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
