9da00e58419c9fdf9a548857ae0b8dc11f5140b1
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-03-29 17:32:07 +0300

    Categories query filter add.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
 
 
 const app = express();
@@ -27,6 +28,8 @@ const port = process.env.PORT || 3000;
 //Routes
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+
 
 
 app.listen(port, () => {
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,5 @@
 const Course = require('../models/Course');
-
+const Category = require('../models/Category');
 exports.createCourse = async (req, res) => {
 
 
@@ -21,9 +21,21 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourse = async (req, res) => {
 
   try {
-    const courses = await Course.find()
+
+    const categorySlug = req.query.categories;
+    const category = await Category.findOne({slug: categorySlug});
+
+    let filter = {};
+
+    if(categorySlug) {
+      filter = { category:category._id };
+    }
+
+    const courses = await Course.find(filter)
+    const categories = await Category.find()
     res.status(200).render('courses', {
       courses,
+      categories,
       page_name: 'courses',
     })
   } catch (err) {
