cb1edcc8755938f4b08cef41191fd0e63750f542
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-03-29 22:21:52 +0300

    login session process.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session')
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -21,14 +22,28 @@ const app = express();
 //Template engine
 app.set('view engine', 'ejs');
 
+//Global variables
+
+global.userIN = null;
+
+
 //Middlewares
 app.use(express.static('public'));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(session({
+  secret: 'my_keyboard_cat',
+  resave: false,
+  saveUninitialized: true,
+}))
 
 const port = process.env.PORT || 3000;
 
 //Routes
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
