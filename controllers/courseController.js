97450fe6046ffacb47da87c103e2de0261a2f4ab
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-03-30 22:26:13 +0300

    last update.

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -4,7 +4,12 @@ const Category = require('../models/Category');
 
 exports.createCourse = async (req, res) => {
     try {
-    const course = await Course.create(req.body);
+    const course = await Course.create({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      user: req.session.userID
+    });
     res.status(201).redirect('/courses')
     } catch (err) {
         res.status(400).json({
@@ -47,7 +52,7 @@ exports.getAllCourse = async (req, res) => {
 exports.getCourse = async (req, res) => {
 
   try {
-    const course = await Course.findOne({slug:req.params.slug})
+    const course = await Course.findOne({slug:req.params.slug}).populate('user');
     res.status(200).render('course', {
       course,
       page_name: 'courses',
