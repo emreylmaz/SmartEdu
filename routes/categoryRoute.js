16bd3837f9cf298a665a25594a4880ea74e30991
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-04-05 19:11:45 +0300

    last update 1/2...

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -4,7 +4,7 @@ const categoryController = require('../controllers/categoryController');
 const router = express.Router();
 
 router.route('/').post(categoryController.createCategory); // Create a category
-
+router.route('/:id').delete(categoryController.deleteCategory); // Delete a category
 
 
 module.exports = router;
